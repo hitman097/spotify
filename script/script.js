d42f3372eb62cbf2746acb845aba1cb012dfc29d
hitman097 <107099128+hitman097@users.noreply.github.com>
2022-08-24 22:51:34 +0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -25,75 +25,28 @@ new Vue({
             url: "https://www.youtube.com/watch?v=Lin-a2lTelg",
             favorited: true
           },
-          {
-            name: "Extreme Ways",
-            artist: "Moby",
-            cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/3.jpg",
-            source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/3.mp3",
-            url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
-            favorited: false
-          },
-          {
-            name: "Butterflies",
-            artist: "Sia",
-            cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/4.jpg",
-            source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/4.mp3",
-            url: "https://www.youtube.com/watch?v=kYgGwWYOd9Y",
-            favorited: false
-          },
-          {
-            name: "The Final Victory",
-            artist: "Haggard",
-            cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/5.jpg",
-            source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/5.mp3",
-            url: "https://www.youtube.com/watch?v=0WlpALnQdN8",
-            favorited: true
-          },
-          {
-            name: "Genius ft. Sia, Diplo, Labrinth",
-            artist: "LSD",
-            cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/6.jpg",
-            source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/6.mp3",
-            url: "https://www.youtube.com/watch?v=HhoATZ1Imtw",
-            favorited: false
-          },
-          {
-            name: "The Comeback Kid",
-            artist: "Lindi Ortega",
-            cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/7.jpg",
-            source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/7.mp3",
-            url: "https://www.youtube.com/watch?v=me6aoX0wCV8",
-            favorited: true
-          },
-          {
-            name: "Overdose",
-            artist: "Grandson",
-            cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/8.jpg",
-            source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/8.mp3",
-            url: "https://www.youtube.com/watch?v=00-Rl3Jlx-o",
-            favorited: false
-          },
-          {
-            name: "Rag'n'Bone Man",
-            artist: "Human",
-            cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/9.jpg",
-            source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/9.mp3",
-            url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
-            favorited: false
-          },
-          {
-            name: "Brown Munde",
-            artist: "AP Dhillon, Gurinder Gill",
-            cover: "https://github.com/mohammadomer691/song/raw/main/size_l.webp  ",
-            source: "https://github.com/mohammadomer691/song/raw/main/Brown-Munde-AP-Dhillon-Gurinder-Gill.mp3",
-            url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
-            favorited: false
-          },
+          ,
           {
             name: "Excuses",
             artist: "ap-dhillon",
             cover: "https://github.com/mohammadomer691/song/raw/main/148549-excuses-ap-dhillon-mp3-song-300.jpg",
             source: "https://github.com/mohammadomer691/song/raw/main/Excuses.mp3",
+            url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
+            favorited: false
+          },
+          {
+            name: "295",
+            artist: "Sidhu Moosewala",
+            cover: "https://raw.githubusercontent.com/mohammadomer691/song/main/295-1.jpg",
+            source: "https://github.com/mohammadomer691/song/blob/main/295_1.mp3?raw=true",
+            url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
+            favorited: false
+          }
+          ,{
+            name: "So High",
+            artist: "Sidhu Moosewala",
+            cover: "https://github.com/mohammadomer691/song/raw/main/so%20high%20png.jpg",
+            source: "https://github.com/mohammadomer691/song/blob/main/So-High-Sidhu-Moose-Wala-190Kbps.mp3?raw=true",
             url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
             favorited: false
           }
